59f89dd51d49622f722bb6811e9780fc0f48cf96
juangomDev <juancarlosgmzany@gmail.com>
2023-09-08 12:21:14 -0400

    v 2.0

--- funct.js
+++ funct.js
@@ -1,51 +1,204 @@
-let snk = {
-    x: 172, y: 175, h: 12, w: 12,
-    rightPressed: false, leftPressed: false,
-    abovePressed: false, downPressed: false,
-    v: 12
+const snake = {
+    x: 50,
+    y: 50,
+    w: 10,
+    h: 10,
+    above: false,
+    down: false,
+    right: false,
+    left: false,
+    speed: 10,
+    body: [],
+    img: "snakeDraw"
 }
 
-let score = 0
-//                x, y, h, w        
-let Walls = [{ x: 0, y: 0, w: 15, h: 400 }, { x: 385, y: 0, w: 15, h: 400 },
-{ x: 0, y: 0, w: 400, h: 15 }, { x: 0, y: 385, w: 400, h: 15 }]
+const wall = [
+    {x: 0,   y: 0,   w: 20,  h: 400,}, // above
+    {x: 0,   y: 0,   w: 400, h: 20,}, // right
+    {x: 0,   y: 380, w: 400, h: 20,}, // down
+    {x: 380, y: 0,   w: 20,  h: 400,}, // left
+    {x: 0,   y: 0,   w: 10,  h: 10,}  // octaculo  
+]
 
-
-let fruit = {x: 0, y: 0, w:12, h:12}
-let brick = {x: 0, y: 0, w:12, h:12}
-
-let header = [], headerCopy = []
-
-function draw(obj, c) {
-    ctx.fillStyle = c
-    ctx.fillRect(obj.x, obj.y, obj.w, obj.h,)
+const elements = {
+    fruit: [],
+    brick: [],
+    score: 0,
+    minute: 3,
+    second: 59
 }
 
+// mover cabeza
+function headMovements() {
+    snake["above"] ? snake.y -= snake.speed :
+        snake["down"] ? snake.y += snake.speed :
+            snake["right"] ? snake.x += snake.speed :
+                snake["left"] ? snake.x -= snake.speed : false
+}
+// hacer que el cuerpo siga los pasos de la cabesa  
+function inheritMovements() {
 
+    snake.body.map( (obj, numero, array) => {
+        if (numero > 0){
+            obj.x = array[numero - 1]
+            obj.y = array[numero - 1]
+        } 
+    } )
+
+    snake.body[0].x = snake.x
+    snake.body[0].y = snake.y
+
+}
+
+// coliciones 
 function collition(x, obj) {
-    let X = obj.x + obj.w,
-        Y = obj.y + obj.h,
-        touch = false
+    let touch = false
 
-    if (X >= x.x && obj.x <= x.x + x.w) {
-        if (Y >= x.y && obj.y <= x.y + x.h) {
-            return touch = true
-        } else { touch = false }
-    } else { touch = false }
+    const vertical = obj.y + obj.h >= x.y && obj.y <= x.y + x.h,
+        horizontal = obj.x + obj.w >= x.x && obj.x <= x.x + x.w
+
+    horizontal ? vertical ? touch = true : touch = false : touch = false
 
     return touch
 }
 
-function clone(obj, address, pos) {
-    let p = { x: obj.x, y: obj.y, w: obj.w, h: obj.h };
+function verifyIntermediate(n) {
+    addressX = snake["right"] || snake["left"]
+    addressY = snake["above"] || snake["down"]
 
-    if (address == "x") {
-        pos == 0 ? p.x = obj.x - obj.h : p.x = obj.x + obj.w
+    function intermediate() {
+        addressX ? snake["right"] ? snake.x += 1 : snake.x -= 1 : false
+        addressY ? snake["down"]  ? snake.y += 1 : snake.y -= 1 : false
+
+        collition(snake, wall[n]) ? true : intermediate()
     }
-    else if (address == "y") {
-        pos == 0 ? p.y = obj.y - obj.h : p.y = obj.y + obj.h
+
+    function preMovements() {
+        const Ghost = { x: snake.x, y: snake.y, w: snake.w, h: snake.h, }
+
+        addressX ? snake["right"] ? Ghost.x += snake.w : Ghost.x -= snake.w : false
+        addressY ? snake["above"] ? Ghost.y -= snake.h : Ghost.y += snake.h : false
+
+        return Ghost
     }
-    return p
+
+    let ghost = preMovements()
+    collition(ghost, wall[n]) ? intermediate() : false
+
+
+}
+
+function elementsCollition() {
+    let b = elements.brick
+        f = elements.fruit
+
+    function posFruit(obj) {
+        obj.x = random(20,360) // cambiar posicion de la fruta X
+        obj.y = random(20,360) // cambiar posicion de la fruta Y
+        snake.body.push( {x: 50, y: 50, w: 10, h: 10, img: snakeDraw, pos: [0, 20]} ) // agrega un elemento del cuerpo nuevo
+        
+        collition(obj, snake)   && posFruit()  // que no aparesca encima de la cabeza
+        collition(obj, wall[4]) && posFruit()  // que no aparesca encima de el 4to muro
+
+        b.map( ( x ) => { collition(obj, x) && posFruit() }) // que no aparesca encima de los octaculos
+        snske.body.map( ( x ) => { collition(obj, x) && posFruit() }) // que no aparesca encima del cuerpo
+            
+        elements.score += 1  // mas puntos
+    }
+
+    function type(type) {
+        type.map( ( x ) => {
+            collition(snake, x) && type.v ? posFruit(x) : false
+        })
+    }
+
+    elements.fruit.length > 0 && type(f)
+    elements.brick.length > 0 && type(b)
+}
+
+// falta coliones con el cuerpo de la serpiente
+function collitionWilkBody() {
+    snake.body.map( (x, namber) => {
+        namber > 0 && collition(x, snake) ? console.log("final"): false
+    } )
+}
+
+
+// elementos  y puntos
+
+const random = (min,max) => (Math.floor(Math.random()*(max - min)) + min);
+
+function fruit() {
+    elements.fruit.push({ 
+        x: random(20,360),  
+        y: random(20,360),  
+        w: 10,  
+        h: 10, 
+        v: true,
+        img: aset,
+        pos: [0, (random(0,1)*10)*2]
+    })
+}
+
+function brick() {
+    elements.brick.push({ 
+        x: random(20,360),  
+        y: random(20,360),  
+        w: 10,  
+        h: 10,  
+        v: false,
+        img: aset,
+        pos: [0, 40]
+    })
+}
+
+function tiempo() {
+    if (elements.second = 0) {
+        elements.minute -= 1
+        elements.second = 60
+    }
+    elements.second -= 1
+}
+
+// dibujar en mantalla
+
+function elementoDraw(obj) {
+    ctx.drawImage(
+        obj.img,
+        obj.pos[0], obj.pos[1],
+        20,20, 
+        obj.x,
+        obj.y,
+        obj.w,
+        obj.h 
+    )
+}
+
+function bodyDraw() {
+    snake.body.map((x, namber) => {
+        namber !== x.length -1 &&
+        ctx.drawImage(
+        x.img,
+        pos[0], pos[1],
+        20,20, 
+        obj.x,
+        obj.y,
+        obj.w,
+        obj.h 
+    )
+    }) 
+
+    function cola() {
+        ctx.drawImage(
+            snakeDraw,
+            pos[0], pos[2],
+            20,20, 
+            obj.x,
+            obj.y,
+            obj.w,
+            obj.h 
+        )
+    }
 }
 
 
@@ -54,37 +207,37 @@ document.addEventListener("keyup", keyUpHandler, false);
 
 // console.log();
 function detener() {
-    snk.rightPressed = false;
-    snk.leftPressed = false;
-    snk.abovePressed = false;
-    snk.downPressed = false;
+    snake.right = false;
+    snake.left = false;
+    snake.above = false;
+    snake.down = false;
 }
 
 function keyDownHandler(e) {
     
 
     if (e.keyCode == 39) { 
-        if (!snk.leftPressed) { 
+        if (!snake.left) { 
             detener();
-            snk.rightPressed = true;  
+            snake.right = true;  
         }
     }
     else if (e.keyCode == 37) { 
-        if (!snk.rightPressed) { 
+        if (!snake.right) { 
             detener();
-            snk.leftPressed  = true; 
+            snake.left  = true; 
         }
     }  
     else if (e.keyCode == 38) { 
-        if (!snk.downPressed) { 
+        if (!snake.down) { 
             detener();
-            snk.abovePressed = true; 
+            snake.above = true; 
         }
     }
     else if (e.keyCode == 40) { 
-        if (!snk.abovePressed) { 
+        if (!snake.above) { 
             detener();
-            snk.downPressed  = true; 
+            snake.down  = true; 
         }
     }
     
@@ -105,19 +258,6 @@ function keyUpHandler(e) {
     //    }
 }
 
-
-
-const random = (min,max) => (Math.floor(Math.random()*(max - min)) + min);
-
-function fruitRan() {
-    fruit.x = random(16, 370)
-    fruit.y = random(16, 370)
-}
-
-function brickRan() {
-    brick.x = random(16, 370)
-    brick.y = random(16, 370)
-}
 
 
 //juangomDev 
--- index.js
+++ index.js
@@ -5,136 +5,56 @@ const ctx = canvas.getContext('2d')
 let fondo = new Image();
 fondo.src = "./img/fondo.jpg"
 
-let wall = new Image();
-wall.src = "./img/wall.jpg"
+let wallDraw = new Image();
+wallDraw.src = "./img/wall.jpg"
 
-let snake = new Image();
-snake.src = "./img/snake.jpg"
+let snakeDraw = new Image();
+snakeDraw.src = "./img/snk.png"
+
+let aset = new Image();
+aset.src = "./img/aset.png"
 
 
 window.onload = () => {
-    ctx.drawImage(wall, 0, 0, 405, 405)
-    ctx.drawImage(fondo, 15, 15, 370, 370)
-    fruitRan()
-    brickRan()
+    ctx.drawImage(wallDraw, 0, 0, 400, 400)
+    ctx.drawImage(fondo, 20, 20, 360, 360)
+    fruit()
+    brick()
 
     function drawloop() {
-        ctx.drawImage(wall, 0, 0, 405, 405)
-        ctx.drawImage(fondo, 15, 15, 370, 370)
-
+        ctx.drawImage(wallDraw, 0, 0, 405, 405)
+        ctx.drawImage(fondo, 20, 20, 360, 360)
+       
 
         //ecena1()
 
 
         function gameOver() {
             clearInterval(loop)
-            //ecena2()
+            ecena2()
         }
 
+        headMovements() 
+        snake.body.length > 0 && inheritMovements()
+        verifyIntermediate(0)
+        verifyIntermediate(1)
+        verifyIntermediate(2)
+        verifyIntermediate(3)
+        elementsCollition()
+        collitionWilkBody()
 
-
-        draw(fruit, "#f00")
-        draw(snk, "#fff")
-        draw(brick, "#00f")
-
-        function norepetir() {
-            fruitRan()
-            collition(brick, fruit) ? norepetir() : false
-            headerCopy.map((x) => {
-                collition(x, fruit) ? norepetir(): false
-            })
-            console.log("HOLA");
-        }
-
-        if (collition(snk, fruit)) {
-
-            norepetir()
-
-            header.push({ x: snk.x, y: snk.y, w: 12, h: 12 })
-            headerCopy.push({ x: snk.x, y: snk.y, w: 12, h: 12 })
-        }
-
-
-
-        if (header.length > 0) {
-            function fru(n) {
-                if (header.length > n) {
-                    header[n] = { x: header[n - 1].x, y: header[n - 1].y, w: 6, h: 6 }
-                    headerCopy[n] = { x: headerCopy[n - 1].x, y: headerCopy[n - 1].y, w: 6, h: 6 }
-                }
-            }
-
-            
-
-            header.map((x, n) => {
-                draw(x, "#000");
-                if (n !== 0) {
-                    fru(header.length - n)
-                    fru(headerCopy.length - n)
-                }
-            })
-            headerCopy[0] = { x: snk.x, y: snk.y, w: 10, h: 10 }
-        }
-
-
-
-        if (snk.rightPressed) {
-
-            if (header.length > 0) {
-                header[0] = {
-                    x: snk.x - 3, y: snk.y + 3, w: 6, h: 6
-                }
-            }
-            if (collition(clone(snk, "x", 1), Walls[1])) {
-                collition(snk, Walls[1]) ? gameOver() : snk.x = 373
-            } else if (collition(clone(snk, "x", 1), brick)) {
-                collition(snk, brick) ? gameOver() : snk.x = brick.x - snk.w
-            } else (snk.x += snk.v)
-
-        }
-        else if (snk.leftPressed) {
-
-            if (header.length > 0) { header[0] = { x: snk.x + 3, y: snk.y + 3, w: 6, h: 6 } }
-
-            if (collition(clone(snk, "x", 0), Walls[0])) {
-                collition(snk, Walls[0]) ? gameOver() : snk.x = 15
-            } else if (collition(clone(snk, "x", 0), brick)) {
-                collition(snk, brick) ? gameOver() : snk.x = brick.x + snk.w
-            } else (snk.x -= snk.v)
-        }
-        else if (snk.abovePressed) {
-
-            if (header.length > 0) { header[0] = { x: snk.x + 3, y: snk.y + 3, w: 6, h: 6 } }
-
-            if (collition(clone(snk, "y", 0), Walls[2])) {
-                collition(snk, Walls[2]) ? gameOver() : snk.y = 15
-            } else if (collition(clone(snk, "y", 0), brick)) {
-                collition(snk, brick) ? gameOver() : snk.y = brick.y + snk.h
-            } else (snk.y -= snk.v)
-        }
-        else if (snk.downPressed) {
-
-            if (header.length > 0) { header[0] = { x: snk.x + 3, y: snk.y - 3, w: 6, h: 6 } }
-
-            if (collition(clone(snk, "y", 1), Walls[3])) {
-                collition(snk, Walls[3]) ? gameOver() : snk.y = 373
-            } else if (collition(clone(snk, "y", 1), brick)) {
-                collition(snk, brick) ? gameOver() : snk.y = brick.y - snk.h
-            } else (snk.y += snk.v)
-        }
-
-
-
-
-
-        header.map((x, n) => {
-            if (n !== 0) {
-                if (collition(snk, x)) { clearInterval(loop) }
-            }
-        })
-
-
-
+        elementoDraw(elements.fruit[0])
+        elementoDraw(elements.brick[0])
+        
+        ctx.drawImage(
+            snakeDraw,
+            0, 0,
+            20,20, 
+            snake.x,
+            snake.y,
+            snake.w,
+            snake.h 
+        )
 
     }
 
